5bf1a82e24d8b859410cf2770eb96f79730994d3
TamNhien <canhthien91@gmail.com>
2024-03-19 20:16:46 +0700

    Add project files.

---
+++ P_GiaiThua.py
@@ -0,0 +1,10 @@
+﻿sonhap = int(input("Nhập số cần tính giai thừa : "))
+giaithua = 1
+
+i = 1
+while i <= sonhap:
+    giaithua *= i
+    i += 1
+
+print(f"Giai thừa của {sonhap} là : {giaithua}")
+
